592d0798f8e74aa911554bd4d6a04b22fc17fdd1
vidya.shevale24@gmail.com <vidya.shevale24@gmail.com>
2026-04-01 21:01:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import User from './User'
+import './App.css'
+
+
+function App() {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <>
+     <h1>Counter Val:{counter}</h1>
+	 <User />
+	 <button onClick={()=>setCounter(counter+1)}>Increase counter value</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/User.jsx
@@ -0,0 +1,4 @@
+function User() {
+    return <h1>User Component</h1>
+}
+export default User;
