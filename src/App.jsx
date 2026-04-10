65ebf4597bc5d3b9d3850530d21d5431893f25e1
vidya.shevale24@gmail.com <vidya.shevale24@gmail.com>
2026-04-10 06:47:29 +0530

    input field value

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import ToDo from './ToDo'
-import Counter from './Counter'
-import Toggle from './Toggle'
 import User from './User'
 
-
 function App() {
-	/* function callFun(){
-		alert("Function called1");
-	} */
-	const [display,setDisplay] =useState(true);
-
-	const toggle = () => {
-		setDisplay(!display);
-	}
-	
-	const userDetails = {
-		'name': 'John Doe',
-		'email': 'john.doe@example.com',
-		'age': 30
-	}
-
+	// const[val, setVal] = useState('vidya');
+	const[name, setName] = useState('');
+	const[pass, setPassword] = useState('');
+	const[email, setEmail] = useState('');
 	return (
 		<>
+			<div>
+				{/* <form action='' method=''> */}
+				{/* <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder='Add name here...' /> */}
+				{/* <button onClick={()=>setVal("")}>Clear</button> */}
 
-			<User userDetail={userDetails} />
-			{/* <button onClick={callFun}>CLick me </button> */}
-			{<button onClick={toggle}>Toggle </button>}
-			{display?<Toggle />:''}
-			<ToDo />
-			<Counter />
-			
+				<input type="text" value={name} placeholder="Enter name" onChange={(event)=>setName(event.target.value)}/>
+				<br/>
+				<input type="password" value={pass} placeholder="Enter password" onChange={(event)=>setPassword(event.target.value)}/>
+				<br/>
+				<input type="text" value={email} placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
+				<br/>
+				<button onClick={()=>{setName('');setPassword('');setEmail('')}}>Clear</button>
+				<h1>{name}</h1>
+				<h1>{pass}</h1>
+				<h1>{email}</h1>
+				{/* </form> */}
+			</div>
 		</>
 	)
-}
-const callFun = () => {
-	alert("Function called1");
 }
 
 export default App
