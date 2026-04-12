5f050160be60e62126bb998df3da495fe543e6c3
vidya.shevale24@gmail.com <vidya.shevale24@gmail.com>
2026-04-12 08:23:18 +0530

    Radio buttons & dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import User from './User'
-import Skills from './Skills'
 
 function App() {
-	// const[val, setVal] = useState('vidya');
-	const[name, setName] = useState('');
-	const[pass, setPassword] = useState('');
-	const[email, setEmail] = useState('');
+	const[gender, setGender] = useState('male');
+	const[city, setCity] = useState('Bangalore');
 	return (
-		<>
 			<div>
-				{/* <form action='' method=''> */}
-				{/* <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder='Add name here...' /> */}
-				{/* <button onClick={()=>setVal("")}>Clear</button> */}
+				<h2>----------------------------------RadioButtons---------------------------------</h2>
+				<input type="radio" onChange={(event)=>setGender(event.target.value)} checked={gender === 'male'} id="male" name="gender" value="male" />
+				<label htmlFor="male">Male</label>
+				<input type="radio" onChange={(event)=>setGender(event.target.value)} checked={gender === 'female'} id="female" name="gender" value="female" />
+				<label htmlFor="female">Female</label>
 
-				{/* <input type="text" value={name} placeholder="Enter name" onChange={(event)=>setName(event.target.value)}/>
-				<br/>
-				<input type="password" value={pass} placeholder="Enter password" onChange={(event)=>setPassword(event.target.value)}/>
-				<br/>
-				<input type="text" value={email} placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
-				<br/>
-				<button onClick={()=>{setName('');setPassword('');setEmail('')}}>Clear</button> */}
-				{/* <h1>{name}</h1>
-				<h1>{pass}</h1>
-				<h1>{email}</h1> */}
-				{/* </form> */}
-				<Skills />
+				<h1>Selected Gender : {gender}</h1>
+
+				<h2>------------------------------------Dropdowns-----------------------------------</h2>
+				<select onChange={(event)=>setCity(event.target.value)} defaultValue={'Bangalore'}>
+					<option value="Delhi">Delhi</option>
+					<option value="Mumbai">Mumbai</option>
+					<option value="Bangalore">Bangalore</option>
+				</select>
+
+				<h1>Selected City : {city}</h1>
 			</div>
-		</>
 	)
 }
 
