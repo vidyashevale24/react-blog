3fac3a0fa3508216e0443c5564355de63090451f
vidya.shevale24@gmail.com <vidya.shevale24@gmail.com>
2026-04-21 06:33:35 +0530

    Map Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 
 function App() {
-	const[gender, setGender] = useState('male');
-	const[city, setCity] = useState('Bangalore');
+	const [gender, setGender] = useState('male');
+	const userdata = [
+		{ id: 1, name: 'John', gender: 'male' },
+		{ id: 2, name: 'Jane', gender: 'female' },
+		{ id: 3, name: 'Doe', gender: 'male' }
+	];
 	return (
-			<div>
-				<h2>----------------------------------RadioButtons---------------------------------</h2>
-				<input type="radio" onChange={(event)=>setGender(event.target.value)} checked={gender === 'male'} id="male" name="gender" value="male" />
-				<label htmlFor="male">Male</label>
-				<input type="radio" onChange={(event)=>setGender(event.target.value)} checked={gender === 'female'} id="female" name="gender" value="female" />
-				<label htmlFor="female">Female</label>
+		<div >
+			<h2>----------------------------------MAP Function/LOOP---------------------------------</h2>
 
-				<h1>Selected Gender : {gender}</h1>
-
-				<h2>------------------------------------Dropdowns-----------------------------------</h2>
-				<select onChange={(event)=>setCity(event.target.value)} defaultValue={'Bangalore'}>
-					<option value="Delhi">Delhi</option>
-					<option value="Mumbai">Mumbai</option>
-					<option value="Bangalore">Bangalore</option>
-				</select>
-
-				<h1>Selected City : {city}</h1>
-			</div>
+			<table border="1">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Gender</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						userdata.map((user) => (
+							<tr key={user.id}>
+								<td>{user.id}</td>
+								<td>{user.name}</td>
+								<td>{user.gender}</td>
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
+		</div>
 	)
 }
 
